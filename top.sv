996ed9551c828393e675b22735fe2e0cb5d3aae5
Flavien Solt <flsolt@ethz.ch>
2024-01-19 11:03:08 +0100

    Simplify the sv source further

--- top.sv
+++ top.sv
@@ -9,5 +9,5 @@ module top(clkin_data, in_data, out_data);
   wire [95:0] out_data;
   assign celloutsig_0z = !(in_data[93] ? in_data[78] : in_data[2]);
   assign celloutsig_1z = { in_data[14], celloutsig_0z } & { in_data[11], celloutsig_0z };
-  assign { out_data[32], out_data[1:0] } = { celloutsig_0z, celloutsig_1z };
+  assign out_data[1:0] = celloutsig_1z;
 endmodule
